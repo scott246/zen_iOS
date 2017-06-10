422814472a1a0883fdd443f3a9440b477b289773
Nathan Scott <scott246@purdue.edu>
2017-06-10 19:51:42 -0400

    basic login page
    non-functional but the layout is there

---
+++ Zen/Zen/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Zen
+//
+//  Created by Nathan Scott on 6/10/17.
+//  Copyright © 2017 Nathan Scott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
