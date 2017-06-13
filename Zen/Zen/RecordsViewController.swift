2465265f2ba131e8312f20591f05537c13324c5c
Nathan Scott <scott246@purdue.edu>
2017-06-13 17:31:19 -0400

    record entry
    can now enter basic records (expenses and incomes) through the records
    screen

--- Zen/Zen/RecordsViewController.swift
+++ Zen/Zen/RecordsViewController.swift
@@ -11,33 +11,45 @@ import UIKit
 import Firebase
 import FirebaseDatabase
 
-
-class Item {
+class Transaction {
     
     var ref: DatabaseReference?
-    var title: String?
+    var date: String?
+    var type: String?
+    var amount: String?
+    //var note: String?
+
     
     init (snapshot: DataSnapshot) {
         ref = snapshot.ref
-        
         let data = snapshot.value as! Dictionary<String, String>
-        title = data["title"]! as String
+        if data["date"] != nil {
+           date = data["date"]! as String
+        }
+        else if data["type"] != nil {
+            type = data["type"]! as String
+        }
+        else if data["amount"] != nil {
+            amount = data["amount"]! as String
+        }
+        //else if data["note"] != nil {
+        //    note = data["note"]! as String
+        //}
     }
     
 }
 
-import UIKit
-import Firebase
-
-class ItemsTableViewController: UITableViewController {
+class RecordsViewController: UITableViewController {
     
     var user: User!
-    var items = [Item]()
+    var transactions = [Transaction]()
     var ref: DatabaseReference!
     private var databaseHandle: DatabaseHandle!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationItem.setLeftBarButtonItems([UIBarButtonItem(title: "Add Income", style: .plain, target: self, action: #selector(didTapAddIncome))], animated: true)
+        self.navigationItem.setRightBarButtonItems([UIBarButtonItem(title: "Add Expense", style: .plain, target: self, action: #selector(didTapAddExpense))], animated: true)
         
         user = Auth.auth().currentUser
         ref = Database.database().reference()
@@ -51,55 +63,113 @@ class ItemsTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+        return transactions.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "recordCell", for: indexPath)
-        let item = items[indexPath.row]
-        cell.textLabel?.text = item.title
+        let transaction = transactions[indexPath.row]
+        cell.textLabel?.text = transaction.amount
+        if transaction.type == "expense"{
+            cell.backgroundColor = UIColor.red
+            cell.textLabel?.textColor = UIColor.white
+        }
+        if transaction.type == "income"{
+            cell.backgroundColor = UIColor.blue
+            cell.textLabel?.textColor = UIColor.white
+        }
         return cell
     }
     
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            let item = items[indexPath.row]
-            item.ref?.removeValue()
+            let transaction = transactions[indexPath.row]
+            transaction.ref?.removeValue()
         }
     }
-
     
-    @IBAction func didTapAddItem(_ sender: UIBarButtonItem) {
-        let prompt = UIAlertController(title: "To Do App", message: "To Do Item", preferredStyle: .alert)
+    func didTapAddIncome() {
+        let prompt = UIAlertController(title: "Add Income", message: "Please enter amount.", preferredStyle: .alert)
         let okAction = UIAlertAction(title: "OK", style: .default) { (action) in
             let userInput = prompt.textFields![0].text
             if (userInput!.isEmpty) {
                 return
             }
-            self.ref.child("users").child(self.user.uid).child("items").childByAutoId().child("title").setValue(userInput)
+            self.ref.child("users").child(self.user.uid).child("transactions").childByAutoId().child("amount").setValue(userInput)
+            self.ref.child("users").child(self.user.uid).child("transactions").childByAutoId().child("type").setValue("income")
+            let date = Date()
+            let formatter = DateFormatter()
+            formatter.dateFormat = "MM-dd-yyyy"
+            let today = formatter.string(from: date)
+            self.ref.child("users").child(self.user.uid).child("transactions").childByAutoId().child("date").setValue(today)
         }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
         prompt.addTextField(configurationHandler: nil)
         prompt.addAction(okAction)
+        prompt.addAction(cancelAction)
         present(prompt, animated: true, completion: nil);
-        
     }
     
-    func startObservingDatabase () {
-        databaseHandle = ref.child("users/\(self.user.uid)/items").observe(.value, with: { (snapshot) in
-            var newItems = [Item]()
-            
-            for itemSnapShot in snapshot.children {
-                let item = Item(snapshot: itemSnapShot as! DataSnapshot)
-                newItems.append(item)
+    func didTapAddExpense() {
+        
+        let prompt = UIAlertController(title: "Add Expense", message: "Please enter amount.", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { (action) in
+            let userInput = prompt.textFields![0].text
+            //let noteInput = prompt.textFields![1].text
+            if (userInput!.isEmpty) {
+                return
             }
-            
-            self.items = newItems
+            self.ref.child("users").child(self.user.uid).child("transactions").childByAutoId().child("amount").setValue(userInput)
+            self.ref.child("users").child(self.user.uid).child("transactions").childByAutoId().child("type").setValue("expense")
+            let date = Date()
+            let formatter = DateFormatter()
+            formatter.dateFormat = "MM-dd-yyyy"
+            let today = formatter.string(from: date)
+            self.ref.child("users").child(self.user.uid).child("transactions").childByAutoId().child("date").setValue(today)
+            //self.ref.child("users").child(self.user.uid).child("transactions").childByAutoId().child("note").setValue(noteInput)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        prompt.addTextField(configurationHandler: nil)
+        prompt.addAction(okAction)
+        prompt.addAction(cancelAction)
+        present(prompt, animated: true, completion: nil);
+    }
+    
+    func startObservingDatabase() {
+        databaseHandle = ref.child("users/\(self.user.uid)/transactions").observe(.value, with: { (snapshot) in
+            var newTransactions = [Transaction]()
+            var counter = 0
+            var transaction: Transaction?
+            for itemSnapShot in snapshot.children {
+                switch counter {
+                case 0:
+                    transaction = Transaction(snapshot: itemSnapShot as! DataSnapshot)
+                    counter += 1
+                    break
+                case 1:
+                    transaction?.type = Transaction(snapshot: itemSnapShot as! DataSnapshot).type
+                    counter += 1
+                    break
+                case 2:
+                    transaction?.date = Transaction(snapshot: itemSnapShot as! DataSnapshot).date
+                    counter += 1
+                    break
+                //case 3:
+                //    transaction?.note = Transaction(snapshot: itemSnapShot as! DataSnapshot).note
+                //    newTransactions.append(transaction!)
+                //    counter = 0
+                default:
+                    break
+                }
+                
+            }
+            self.transactions = newTransactions
             self.tableView.reloadData()
             
         })
     }
     
     deinit {
-        ref.child("users/\(self.user.uid)/items").removeObserver(withHandle: databaseHandle)
+        ref.child("users/\(self.user.uid)/transactions").removeObserver(withHandle: databaseHandle)
     }
 }
